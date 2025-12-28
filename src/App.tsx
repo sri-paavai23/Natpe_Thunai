617f5d6d2fd7e438b668e9b4d751bda8e66d6757
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-28 20:11:54 +0530

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import NotFound from "./pages/NotFound";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
-
+export default App;
 // ... rest of your code
