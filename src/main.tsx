61021adb9c3984ad9e1aa43842b25b76ec7d8f2d
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-28 19:42:23 +0530

    [dyad] Fixing CSS import error - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import './index.css';
+import '../index.css'; // Update the import statement to point to the correct location
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
