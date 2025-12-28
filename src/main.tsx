fb8410aaa83d1af7187a72293c6ec6ee8b71a916
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-28 20:11:03 +0530

    Updated src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import {App} from "./App.tsx";
+import App from "./App.tsx";
 import "./globals.css";
 import { ThemeProvider } from "next-themes"; // Import ThemeProvider
 
@@ -8,3 +8,4 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </ThemeProvider>
 );
+export default App;
